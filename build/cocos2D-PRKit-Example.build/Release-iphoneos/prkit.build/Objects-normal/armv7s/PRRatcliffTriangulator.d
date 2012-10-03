d40955d24f283d6187f77a85f8d5ffed2c751509
Panajev <Panajev@gmail.com>
2012-10-03 15:25:06 +0200

    (x): local script fixes.

--- build/cocos2D-PRKit-Example.build/Release-iphoneos/prkit.build/Objects-normal/armv7s/PRRatcliffTriangulator.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/PRKit/PRRatcliffTriangulator.mm \
-  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/prkit/PRRatcliffTriangulator.h \
-  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/prkit/PRTriangulator.h \
-  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/prkit/triangulate.h
--- build/cocos2D-PRKit-Example.build/Release-iphonesimulator/prkit.build/Objects-normal/i386/triangulate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/PRKit/triangulate.cpp \
-  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/prkit/triangulate.h
