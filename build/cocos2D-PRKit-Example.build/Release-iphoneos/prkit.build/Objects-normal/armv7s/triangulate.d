d81f715319a10d4925d5e69d6c667c61fbd436f7
Panajev <Panajev@gmail.com>
2012-10-03 15:19:11 +0200

    (+): added static library target for PRKit.

---
+++ build/cocos2D-PRKit-Example.build/Release-iphoneos/prkit.build/Objects-normal/armv7s/triangulate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/PRKit/triangulate.cpp \
+  /Users/Panajev/Programming/workspace/iPhoneTest/iPad_Projects/ExternalSources/cocos2d-PRKit/prkit/triangulate.h
